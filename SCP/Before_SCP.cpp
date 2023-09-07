ca2cff5bff39a52f07ab00508a428863da249296
Virendra Haratlia <141754196+Virendra-Haratlia@users.noreply.github.com>
2023-09-07 10:42:06 +0530

    Update Before_SCP.cpp

--- SCP/Before_SCP.cpp
+++ SCP/Before_SCP.cpp
@@ -1,1 +1,34 @@
 
+#include <iostream>
+class Shape
+{
+public:
+    virtual void OpenGLDraw() = 0;
+    virtual void VulkanDraw() = 0;
+    virtual ~Shape() = default;
+};
+class Rectangle :public Shape
+{
+public:
+    void OpenGLDraw() override{ std::cout << "Rectangle::OpenGLDraw"<<std::endl; };
+    void VulkanDraw() override { std::cout << "Rectangle::VulkanDraw"<< std::endl; };
+};
+#include <iostream>
+
+int main()
+{
+    Rectangle rect;
+    rect.OpenGLDraw();
+    rect.VulkanDraw();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
